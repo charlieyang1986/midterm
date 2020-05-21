5023f494613c7f683d386de2f8a72f0697b08e1d
Chunhai Yang <cyang21@mail.greenriver.edu>
2020-05-21 00:33:20 -0400

    index.php and views revised

--- index.php
+++ index.php
@@ -26,12 +26,34 @@ $f3->route('GET|POST /survey', function($f3) {
 
       //$options = getOptions();
 
-    $options = array("this midterm is easy", "i like midterms", "today is Monday");
+    $options = array("This midterm is easy", "I like midterms", "Today is Monday");
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //Store the data in the session array
+        $_SESSION['name'] = $_POST['name'];
+        $_SESSION['options'] = $_POST['options'];
+
+
+        //Redirect to summary page
+        $f3->reroute('summary');
+    }
+
 
     $f3->set('options', $options);
     $view = new Template();
     echo $view->render('views/survey.html');
 
+});
+
+//summary route
+$f3->route('GET /summary', function() {
+
+    $view = new Template();
+    echo $view->render('views/summary.html');
+
+    session_destroy();
 });
 
 
