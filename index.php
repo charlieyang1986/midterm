319c7a3c14aad5b2bcbc00d0b65f459f18161a58
Chunhai Yang <cyang21@mail.greenriver.edu>
2020-05-20 21:01:45 -0400

    survey route and survey page added

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
 
 //Require the autoload file
 require_once("vendor/autoload.php");
+//require_once("models/data-layer.php");
 
 
 //Instantiate the F3 Base class
@@ -17,6 +19,19 @@ $f3->route('GET /', function() {
 
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+//survey route
+$f3->route('GET|POST /survey', function($f3) {
+
+      //$options = getOptions();
+
+    $options = array("this midterm is easy", "i like midterms", "today is Monday");
+
+    $f3->set('options', $options);
+    $view = new Template();
+    echo $view->render('views/survey.html');
+
 });
 
 
