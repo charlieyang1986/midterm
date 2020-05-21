71a46631b119a50d6225f6e66f15663a6b8394b3
Chunhai Yang <cyang21@mail.greenriver.edu>
2020-05-21 02:15:11 -0400

    index and views revised

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ session_start();
 
 //Require the autoload file
 require_once("vendor/autoload.php");
+require_once("models/validate.php");
+
 //require_once("models/data-layer.php");
 
 
@@ -28,20 +30,44 @@ $f3->route('GET|POST /survey', function($f3) {
 
     $options = array("This midterm is easy", "I like midterms", "Today is Monday");
 
+
     //If the form has been submitted
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        //Store the data in the session array
-        $_SESSION['name'] = $_POST['name'];
-        $_SESSION['options'] = $_POST['options'];
+        //Validate the data
+//        if (empty($_POST['name']) || !in_array($_POST['options'], $options)) {
+//            echo "<p>Please enter your name and select an option</p>";
+//        }
 
+          if(!validName($_POST['name'])){
 
-        //Redirect to summary page
-        $f3->reroute('summary');
+              // set an error variable in the F3 hive
+              $f3->set('errors["name"]', "Enter a valid name");
+          }
+
+          if(!isset($_POST['options'])){
+
+              // set an error variable in the F3 hive
+              $f3->set('errors["options"]',"Please select at least one");
+          }
+          if(empty($f3->get('errors'))){
+              //Store the data in the session array
+              $_SESSION['name'] = $_POST['name'];
+              $_SESSION['options'] = $_POST['options'];
+
+              //Redirect to summary page
+              $f3->reroute('summary');
+              session_destroy();
+          }
+
     }
 
 
     $f3->set('options', $options);
+    $f3->set('name', $_POST['name']);
+    $f3->set('option', $_POST['options']);
+
+
     $view = new Template();
     echo $view->render('views/survey.html');
 
@@ -53,7 +79,6 @@ $f3->route('GET /summary', function() {
     $view = new Template();
     echo $view->render('views/summary.html');
 
-    session_destroy();
 });
 
 
